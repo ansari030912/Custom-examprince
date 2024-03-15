20c772be754122fa5cd4bfc0269832e7134ad62f
Hussnain <howrex95@gmail.com>
2024-03-15 19:54:32 +0500

    ..

---
+++ app/components/Cards/ForgotPasswordCard.jsx
@@ -0,0 +1,170 @@
+"use client";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import { Alert, Snackbar } from "@mui/material";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import loginAuth from "../auth/LoginAuth";
+
+export const metadata = {
+  title: "Forgot password",
+  description: "A world of css",
+  keywords: "A world of css",
+  robots: "noIndex",
+};
+const ForgotPasswordCard = () => {
+  const [passwordRest, setPasswordRest] = useState({});
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [ip, setIp] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const fetchIP = async () => {
+    try {
+      const response = await axios.get("https://api.dumpsboss.com/v1/my-ip", {
+        headers: {
+          "x-api-key": "ed79766c-2cc1-4967-8d3c-035387603caf",
+        },
+      });
+      setIp(response.data);
+    } catch (error) {
+      console.error("Error fetching IP:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIP();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setEmailError("");
+
+    if (!email) {
+      setEmailError("Email is required");
+    }
+    if (emailError) {
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "https://api.dumpsboss.com/v1/account/forgot-password",
+        {
+          email,
+          ip,
+          reset_url: "http://localhost:3000/reset-password/",
+        },
+        {
+          headers: {
+            "x-api-key": "ed79766c-2cc1-4967-8d3c-035387603caf",
+          },
+        }
+      );
+      setPasswordRest(response.data);
+      setOpenSnackbar(true);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Something went wrong. Please try again later.");
+    }
+  };
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <div className="mx-auto max-w-8xl flex justify-center">
+              <div className="md:mx-150px lg:mt-2 rounded-2">
+                <img
+                  src="/banner.png"
+                  alt="Banner"
+                  loading="lazy"
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <>
+              <form onSubmit={handleSubmit} className="lg:mt-12">
+                <div className="flex ">
+                  <div className="flex flex-row w-full">
+                    <div className="flex flex-1 justify-center px-4 relative">
+                      <div className="flex flex-1 flex-col  justify-center space-y-5 max-w-md">
+                        <div className="flex flex-col space-y-2 text-center">
+                          <h2 className="text-3xl md:text-4xl font-bold">
+                            Fotgot your account
+                          </h2>
+                          <p className="text-md md:text-xl">
+                            Enter your email and press forgot password to get a
+                            rest verification code.
+                          </p>
+                        </div>
+                        <div className="flex flex-col max-w-md space-y-5">
+                          <input
+                            type="email"
+                            placeholder="Email"
+                            className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+                            value={email}
+                            onChange={(e) => {
+                              setEmail(e.target.value), setEmailError("");
+                            }}
+                          />
+                          {emailError && (
+                            <span style={{ color: "red" }} className="text-sm">
+                              {emailError}
+                            </span>
+                          )}
+                          <button
+                            type="submit"
+                            className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white"
+                          >
+                            Forgot password
+                          </button>
+                          <div className="flex justify-center items-center">
+                            <span className="w-full border border-black"></span>
+                            <span className="px-4">Or</span>
+                            <span className="w-full border border-black"></span>
+                          </div>
+                          <Link
+                            href={"/sign-in"}
+                            className="flex items-center hover:bg-white justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black relative"
+                          >
+                            <button>
+                              <span>Back to sign in</span>
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              <Snackbar
+                open={openSnackbar}
+                autoHideDuration={6000}
+                onClose={() => setOpenSnackbar(false)}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+              >
+                <Alert
+                  onClose={() => setOpenSnackbar(false)}
+                  severity={passwordRest.email_sent ? "success" : "error"}
+                  variant="filled"
+                  sx={{ width: "100%" }}
+                >
+                  {passwordRest.message && passwordRest.message}
+                </Alert>
+              </Snackbar>
+            </>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+export default ForgotPasswordCard;
