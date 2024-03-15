329ab526f3adfd7393acde21750d85e038833a05
Hussnain <howrex95@gmail.com>
2024-03-15 19:24:09 +0500

    ..

--- app/download-history/page.jsx
+++ app/download-history/page.jsx
@@ -1,12 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-"use client";
-import { Container, Grid } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import DownloadHistoryTable from "../components/Tables/DownloadHistoryTable";
+
+import withAuth from "../auth/RouterAuth";
+import DownloadHistoryCard from "../components/Cards/DownloadHistoryCard";
+
 
 export const metadata = {
   title: "Download History",
@@ -15,164 +12,10 @@ export const metadata = {
   robots: "noIndex",
 };
 const DownloadHistoryPage = () => {
-  const [user, setUser] = useState();
-  const router = useRouter();
-  useEffect(() => {
-    const loginResponse = JSON.parse(localStorage.getItem("loginResponse"));
-    if (!loginResponse?.is_active) {
-      return router.push("/sign-in");
-    }
-    setUser(loginResponse);
-  }, [router]);
+
   return (
-    <>
-      <Container>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <div className="mx-auto max-w-8xl flex justify-center">
-              <div className="md:mx-150px lg:mt-2 rounded-2">
-                <img
-                  src="/banner.png"
-                  alt="banner"
-                  loading="lazy"
-                  width="100%"
-                  height="100%"
-                />
-              </div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <DownloadHistoryTable />
-          </Grid>
-          <Grid item xm={12} md={4}>
-            <Grid
-              container
-              className="text-white bg-gradient-to-br from-gray-800 to-blue-400"
-              sx={{
-                display: "flex",
-                mt: "10px",
-                width: "100%",
-              }}
-            >
-              <Grid item xs={12} sx={{ padding: "10px" }}>
-                <Typography
-                  component="div"
-                  variant="h5"
-                  color={"white"}
-                  sx={{ borderBottom: "1px solid white" }}
-                >
-                  Refund Policy
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={8}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "10px",
-                  mt: "-12px",
-                }}
-              >
-                <Typography
-                  variant="subtitle1"
-                  color={"white"}
-                  component="div"
-                  fontSize={12}
-                >
-                  DumpsArena.com has a remarkable success record. We're
-                  confident of our products and provide a no hassle refund
-                  policy.
-                </Typography>
-                <Link
-                  href="/refund-policy"
-                  className="text-white underline  hover:text-blue-400"
-                >
-                  How our refund policy works?
-                </Link>
-              </Grid>
-              <Grid
-                item
-                xs={4}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  mt: "-12px",
-                }}
-              >
-                <img
-                  width="200px"
-                  height="200px"
-                  src="/money_back.png"
-                  alt="Live from space album cover"
-                />
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              className="text-white bg-gradient-to-t from-gray-800 to-blue-400"
-              sx={{
-                display: "flex",
-                mt: "10px",
-                width: "100%",
-              }}
-            >
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  padding: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  width="100%"
-                  src="safe_checkout_optimized.png"
-                  alt="safe_checkout_optimized"
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "10px",
-                  mt: "-12px",
-                }}
-              >
-                <Typography
-                  variant="subtitle1"
-                  color={"white"}
-                  component="div"
-                  fontSize={12}
-                  style={{ textAlign: "justify" }}
-                >
-                  Your purchase with DumpsArena.com is safe and fast. The
-                  DumpsArena.com website is protected by 256-bit SSL from
-                  Cloudflare, the leader in online security.
-                </Typography>
-                <Link
-                  type="button"
-                  href="/"
-                  className="text-white underline  hover:text-blue-400 mt-2"
-                >
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-blue-400 to-gray-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  >
-                    Customer Support
-                  </button>
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
+    <DownloadHistoryCard />
   );
 };
 
-export default DownloadHistoryPage;
+export default withAuth(DownloadHistoryPage);
