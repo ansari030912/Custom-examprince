3291c954eb4f78a4e3815321836022d597b1af61
Hussnain <howrex95@gmail.com>
2024-04-15 10:24:48 +0500

    ...

--- app/robots.jsx
+++ app/robots.jsx
@@ -3,7 +3,13 @@ export default function robots() {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: "/sign-in",
+      disallow: "/sign-up",
+      disallow: "/check-out",
+      disallow: "/download-history",
+      disallow: "/login-history",
+      disallow: "/reset-password",
+      disallow: "/forgot-password",
     },
     sitemap: "https://examprince.com/sitemap.xml",
   };
