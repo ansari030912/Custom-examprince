d4486d480f1dcfcf27c92100e68645e5449383fb
Hussnain <howrex95@gmail.com>
2024-04-15 10:35:05 +0500

    .

--- app/robots.jsx
+++ app/robots.jsx
@@ -4,12 +4,6 @@ export default function robots() {
       userAgent: "*",
       allow: "/",
       disallow: "/sign-in",
-      disallow: "/sign-up",
-      disallow: "/check-out",
-      disallow: "/download-history",
-      disallow: "/login-history",
-      disallow: "/reset-password",
-      disallow: "/forgot-password",
     },
     sitemap: "https://examprince.com/sitemap.xml",
   };
