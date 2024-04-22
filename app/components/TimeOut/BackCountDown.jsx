243e6a9a0c822dd4fc3042e64dd0dc0a6a325f24
Hussnain <howrex95@gmail.com>
2024-04-22 12:51:10 +0500

    ...

--- app/components/TimeOut/BackCountDown.jsx
+++ app/components/TimeOut/BackCountDown.jsx
@@ -6,20 +6,12 @@ function BackCountDown() {
   const initialDuration = 16 * 60 * 60 * 1000; // 16 hours in milliseconds
   const [duration, setDuration] = useState(initialDuration);
 
-  // Function to retrieve start time from local storage
-  const getStartTimeFromLocalStorage = () => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      return localStorage.getItem("countdownStartTime");
-    }
-    return null;
-  };
-
   // Get the start time from localStorage or set it to the current time
-  const startTime = getStartTimeFromLocalStorage()
-    ? parseInt(getStartTimeFromLocalStorage())
+  const startTime = localStorage.getItem("countdownStartTime")
+    ? parseInt(localStorage.getItem("countdownStartTime"))
     : new Date().getTime();
 
-  // Function to calculate remaining time based on start time and duration
+  // Calculate the remaining time based on the start time and duration
   const calculateRemainingTime = () => {
     const currentTime = new Date().getTime();
     let remainingTime = duration - (currentTime - startTime);
@@ -28,10 +20,7 @@ function BackCountDown() {
     if (remainingTime <= 0) {
       remainingTime = initialDuration;
       setDuration(initialDuration);
-
-      if (typeof window !== "undefined" && window.localStorage) {
-        localStorage.setItem("countdownStartTime", currentTime.toString());
-      }
+      localStorage.setItem("countdownStartTime", currentTime.toString());
     }
 
     return remainingTime;
