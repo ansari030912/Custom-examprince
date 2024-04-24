4ccb540fa1936aa6ce158c21c8e8617342410385
Hussnain <howrex95@gmail.com>
2024-04-24 11:52:25 +0500

    ;.

--- app/refund-policy/page.jsx
+++ app/refund-policy/page.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
-import { Card, Container, Grid, Typography } from "@mui/material";
-import Link from "next/link";
+import { Card, Container, Grid } from "@mui/material";
+import SafeCheckOutCard from "../components/Cards/SafeCheckOutCard";
 import WindowsCard from "../components/Cards/WindowsCard";
 import HotExamsData from "../components/Tables/HotExamsData";
 
@@ -40,7 +40,7 @@ const RefundPolicy = () => {
                 />
               </div>
             </div>
-            <Card className="text-white bg-gradient-to-r from-blue-400 to-gray-900 p-2.5 font-bold text-2xl">
+            <Card className="text-white bg-gradient-to-l from-blue-400 to-gray-900 p-2.5 font-bold text-2xl">
               <div style={{ color: "white" }}>ExamPrince.Com Refund Policy</div>
             </Card>
             <Card className="p-6 mt-2">
@@ -144,66 +144,7 @@ const RefundPolicy = () => {
           </Grid>
           <Grid item sm={12} lg={4}>
             <HotExamsData />
-
-            <Grid
-              container
-              className="text-white bg-gradient-to-t from-gray-800 to-blue-400"
-              sx={{
-                display: "flex",
-                mt: "10px",
-                width: "100%",
-              }}
-            >
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  padding: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  width="100%"
-                  src="/safe_checkout_optimized.png"
-                  alt="safe_checkout_optimized"
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "10px",
-                  mt: "-12px",
-                }}
-              >
-                <Typography
-                  variant="subtitle1"
-                  color={"white"}
-                  component="div"
-                  fontSize={12}
-                  style={{ textAlign: "justify" }}
-                >
-                  Your purchase with ExamPrince.com is safe and fast. The
-                  ExamPrince.com website is protected by 256-bit SSL from
-                  Cloudflare, the leader in online security.
-                </Typography>
-                <Link
-                  type="button"
-                  href="/"
-                  className="text-white underline  hover:text-blue-400 mt-2"
-                >
-                  <button
-                    type="button"
-                    className="text-white bg-gradient-to-r from-blue-400 to-gray-600 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  >
-                    Customer Support
-                  </button>
-                </Link>
-              </Grid>
-            </Grid>
+            <SafeCheckOutCard />
           </Grid>
         </Grid>
       </Container>
