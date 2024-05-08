a22599d0815cad57eb14fd770c511b770c87fda2
Hussnain <howrex95@gmail.com>
2024-05-08 11:17:05 +0500

    ..

---
+++ app/certifications-2/sitemap.jsx
@@ -0,0 +1,25 @@
+export default async function sitemap() {
+  const response = await fetch(
+    "https://certsgang.com/v1/sitemap/certification/2",
+    {
+      headers: {
+        "x-api-key": "ed79766c-2cc1-4967-8d3c-035387603caf",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  return data?.certs?.map((item) => ({
+    url: `https://examprince.com/certifications/${item?.cert_perma.replace(
+      /&/g,
+      "&amp;"
+    )}`,
+    lastModified: "2024-04-17",
+    priority: 0.6,
+  }));
+}
