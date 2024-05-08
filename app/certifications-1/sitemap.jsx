1279f7072585a26c0cca2eecfc493bedfab9d637
Hussnain <howrex95@gmail.com>
2024-05-08 11:30:25 +0500

    ..

--- app/certifications-1/sitemap.jsx
+++ app/certifications-1/sitemap.jsx
@@ -1,22 +1,21 @@
 export default async function sitemap() {
-  const response = await fetch(
-    "https://certsgang.com/v1/sitemap/certification/1",
-    {
+    const response = await fetch("https://certsgang.com/v1/sitemap/vendors", {
       headers: {
         "x-api-key": "ed79766c-2cc1-4967-8d3c-035387603caf",
       },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  );
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
+  
+    const data = await response.json();
+    
+  
+    return data.map((item) => ({
+      url: `https://examprince.com/certifications/${item.vendor_perma}`,
+      lastModified: "2024-04-17",
+      priority: 0.6,
+    }));
   }
-
-  const data = await response.json();
-
-  return data?.certs?.map((item) => ({
-    url: `https://examprince.com/certifications/${item?.vendor_perma}`,
-    lastModified: "2024-04-17",
-    priority: 0.6,
-  }));
-}
+  
